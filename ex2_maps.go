1c15d212641efc395965fe8fcbbdbcedbf04b943
Christi Kazakov <ckazakov18@gmail.com>
2018-07-05 22:20:26 -0600

    Add exercise 2 on maps

---
+++ ex2_maps.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	str := strings.Fields(s)
+	ret := make(map[string]int)
+	for i := 0 i < len(str); i++ {
+		ret[str[i]]
+		i++
+	}
+	return ret
+}
+
+// return a map of the counts of each word in the string
+func main() {
+	wc.Test(WordCount)
+}
